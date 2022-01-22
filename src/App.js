9311f0ce843429a3dea9347345d8bc193ec7f4b1
aichajebri <77939143+aichajebri@users.noreply.github.com>
2022-01-22 16:23:23 +0000

    my 1st jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import myimage from "./imageInSrc.jpg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+      <h1 className='title red'>Aisha Jebri</h1>
+      <br/>
+        <img src={myimage} alt="error"/>
+        <br/>
+        <h2 className="sub">Blue Nebula</h2>
+        <br/>
+        <img src={'/imageInPublic.jpg'} alt="error" />
+        <br/>
+        <h3 className="sub">Volcano </h3>
+        <br/>
+        <video width="320" height="240" controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+    </div>
+   </center>
     </div>
   );
 }
-
 export default App;
